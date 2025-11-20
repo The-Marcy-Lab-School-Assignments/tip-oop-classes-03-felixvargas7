eb0236ce5aab8b7e7c62229cc6869d2d190727a7
Felix Vargas <felix70529@gmail.com>
2025-11-20 12:13:24 -0500

    My own class for bags

---
+++ example.js
@@ -0,0 +1,45 @@
+class Bag {
+    size() {
+        return `normal`
+    }
+    color() {
+        return `black`
+    }
+
+}
+
+class Hikingbag {
+    size() {
+        return `large`
+    }
+    color() {
+        return `camoflauge`
+    }
+}
+
+class Schoolbag {
+    size() {
+        return `small`
+    }
+    color() {
+        return `blue`
+    }
+}
+
+class Duffelbag {
+    size() {
+        return `extra large`
+    }
+    color() {
+        return `green`
+    }
+}
+
+class Minibackpack {
+    size() {
+        return `extra small`
+    }
+    color() {
+        return `pink`
+    }
+}
